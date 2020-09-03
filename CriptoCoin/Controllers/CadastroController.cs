70664b7f24f38f8ad1e60dd4228d70423c352b79
Paulo Victor Alves de Sousa <paulo.asousa@ADM.UNIP.BR>
2020-09-02 22:53:56 -0300

    Criado tela de cadastro

---
+++ CriptoCoin/Controllers/CadastroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CriptoCoin.Controllers
+{
+    public class CadastroController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
