82223b7ff1c5ae35a6caba78ff5ed4cbdcc85591
Paulo Victor Alves de Sousa <paulo.asousa@ADM.UNIP.BR>
2020-09-12 12:56:22 -0300

    Publicando projeto para o grupo do PIM

--- CriptoCoin/App_Start/RouteConfig.cs
+++ CriptoCoin/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CriptoCoin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
