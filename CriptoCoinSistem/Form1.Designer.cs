61fd8f5595ab4cbf92abe8855815d55b8cec74bb
Paulo Victor Alves de Sousa <paulo.asousa@ADM.UNIP.BR>
2020-09-15 21:44:16 -0300

    Teste Diego medeiros

--- CriptoCoinSistem/Form1.Designer.cs
+++ CriptoCoinSistem/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -65,11 +66,22 @@
             this.button2.Text = "Usuario";
             this.button2.UseVisualStyleBackColor = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(461, 55);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DIEGO MEDEIROS";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -85,6 +97,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
