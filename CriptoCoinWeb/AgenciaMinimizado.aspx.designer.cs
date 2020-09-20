2b72d7981f3843da37b2c2714667cd8cf414e46d
Wash20 <washingtonlimadasilva9@gmail.com>
2020-09-20 02:20:05 -0300

    Tela Agencia minimizada ja com html e css quase acabado, faltando alinhar palavras, arrastar o bloco preto mais para esquerda, ajustar imagem e o que colcoar no campo vazio.

---
+++ CriptoCoinWeb/AgenciaMinimizado.aspx.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace CriptoCoinWeb
+{
+
+
+    public partial class AgenciaMinimizado
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
